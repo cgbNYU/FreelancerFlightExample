5e33da55aa4492c5d13c5b024635bd945e7c54dd
cgbNYU <cgb316@nyu.edu>
2019-03-11 16:32:12 -0400

    Added probuilder to the project.
    Added an enemy that can detect player with Dot product and output debug log

---
+++ Assets/FreelancerFlightExample/Scripts/Enemies/ViewCone.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Checks to see if the enemy can see the player and turns the enemy to a hostile state if so
+public class ViewCone : MonoBehaviour
+{
+    
+    //Hold onto player transform
+    private Transform playerTransform;
+    
+    //Public variables
+    public float viewAngle = 30;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerTransform = GameObject.Find("PlayerShip").transform; //initialize playerTransform
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ViewCheck();
+    }
+
+    //Checks to see if the player is within a cone of view width viewAngle degrees
+    private void ViewCheck()
+    {
+        var cone = Mathf.Cos(viewAngle * Mathf.Deg2Rad);
+        var heading = (playerTransform.position - transform.position).normalized;
+
+        if (Vector3.Dot(transform.up, heading) > cone)
+        {
+            Debug.Log("Player Spotted!");
+        }
+    }
+}
